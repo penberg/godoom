7128f1e1e76821f84870887506f806654ff35dba
Pekka Enberg <penberg@iki.fi>
2014-11-18 15:59:02 +0200

    Triangle
    Signed-off-by: Pekka Enberg <penberg@iki.fi>

--- godoom.go
+++ godoom.go
@@ -1,10 +1,34 @@
 package main
 
 import (
-	"fmt"
 	"github.com/codegangsta/cli"
 	"github.com/penberg/godoom/wad"
 	"os"
+	"fmt"
+	"github.com/go-gl/gl"
+	glfw "github.com/go-gl/glfw3"
+	"github.com/go-gl/glu"
+	"runtime"
+)
+
+const (
+        vertex = `#version 330
+
+in vec2 position;
+
+void main()
+{
+    gl_Position = vec4(position, 0.0, 1.0);
+}`
+
+        fragment = `#version 330
+
+out vec4 outColor;
+
+void main()
+{
+    outColor = vec4(1.0, 1.0, 1.0, 1.0);
+}`
 )
 
 const (
@@ -106,6 +130,88 @@ func main() {
 			Y: player1.YPosition,
 		}
 		traverseBsp(level, position, len(level.Nodes)-1)
+		game()
 	}
 	app.Run(os.Args)
+}
+
+func game() {
+	runtime.LockOSThread()
+
+	glfw.Init()
+	defer glfw.Terminate()
+
+	glfw.WindowHint(glfw.ContextVersionMajor, 3)
+	glfw.WindowHint(glfw.ContextVersionMinor, 3)
+	glfw.WindowHint(glfw.OpenglForwardCompatible, glfw.True)
+	glfw.WindowHint(glfw.OpenglProfile, glfw.OpenglCoreProfile)
+
+	window, err := glfw.CreateWindow(800, 600, "Example", nil, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	defer window.Destroy()
+
+	window.MakeContextCurrent()
+	glfw.SwapInterval(1)
+
+	gl.Init()
+
+	vao := gl.GenVertexArray()
+	vao.Bind()
+
+	vbo := gl.GenBuffer()
+	vbo.Bind(gl.ARRAY_BUFFER)
+
+	verticies := []float32{0, 1, 0, -1, -1, 0, 1, -1, 0}
+
+	gl.BufferData(gl.ARRAY_BUFFER, len(verticies)*4, verticies, gl.STATIC_DRAW)
+
+	vertex_shader := gl.CreateShader(gl.VERTEX_SHADER)
+	vertex_shader.Source(vertex)
+	vertex_shader.Compile()
+	fmt.Println(vertex_shader.GetInfoLog())
+	defer vertex_shader.Delete()
+
+	fragment_shader := gl.CreateShader(gl.FRAGMENT_SHADER)
+	fragment_shader.Source(fragment)
+	fragment_shader.Compile()
+	fmt.Println(fragment_shader.GetInfoLog())
+	defer fragment_shader.Delete()
+
+	program := gl.CreateProgram()
+	program.AttachShader(vertex_shader)
+	program.AttachShader(fragment_shader)
+
+	program.BindFragDataLocation(0, "outColor")
+	program.Link()
+	program.Use()
+	defer program.Delete()
+
+	positionAttrib := program.GetAttribLocation("position")
+	positionAttrib.AttribPointer(3, gl.FLOAT, false, 0, nil)
+	positionAttrib.EnableArray()
+	defer positionAttrib.DisableArray()
+
+	gl.ClearColor(0.3, 0.3, 0.3, 1.0)
+
+	for !window.ShouldClose() {
+		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+		gl.DrawArrays(gl.TRIANGLES, 0, 3)
+
+		window.SwapBuffers()
+		glfw.PollEvents()
+
+		if window.GetKey(glfw.KeyEscape) == glfw.Press {
+			window.SetShouldClose(true)
+		}
+	}
+}
+
+func checkGLerror() {
+	if glerr := gl.GetError(); glerr != gl.NO_ERROR {
+		string, _ := glu.ErrorString(glerr)
+		panic(string)
+	}
 }
