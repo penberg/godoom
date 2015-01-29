69f365d3e7f93d6925641d2da1e66fe613ed127c
Pekka Enberg <penberg@iki.fi>
2015-01-29 17:57:31 +0200

    Render level in 3D
    Render the levels in 3D by using the 2D Y coordinate as 3D Z coordinate
    and calculating 3D Y coordinate using sector floor and ceiling heights.
    Upper and lower texture use the same heights from the opposite sector.
    
    Also implement camera movement so that the player is able to walk around
    the level.
    
    Signed-off-by: Pekka Enberg <penberg@iki.fi>

--- godoom.go
+++ godoom.go
@@ -5,6 +5,7 @@ import (
 	"github.com/codegangsta/cli"
 	"github.com/go-gl/gl"
 	glfw "github.com/go-gl/glfw3"
+	"github.com/go-gl/mathgl/mgl32"
 	"os"
 	"runtime"
 )
@@ -12,11 +13,13 @@ import (
 const (
 	vertex = `#version 330
 
-in vec2 position;
+in vec3 vertex;
+
+uniform mat4 MVP;
 
 void main()
 {
-    gl_Position = vec4(position, 0.0, 1.0);
+    gl_Position = MVP * vec4(vertex, 1.0);
 }`
 
 	fragment = `#version 330
@@ -33,12 +36,18 @@ const (
 	subsectorBit = int(0x8000)
 )
 
+type Point3 struct {
+	X int16
+	Y int16
+	Z int16
+}
+
 type Point struct {
 	X int16
 	Y int16
 }
 
-func renderSubsector(level *Level, idx int, vertices []Vertex) []Vertex {
+func renderSubsector(level *Level, idx int, vertices []Point3) []Point3 {
 	ssector := level.SSectors[idx]
 	for seg := ssector.StartSeg; seg < ssector.StartSeg+ssector.Numsegs; seg++ {
 		vertices = append(vertices, renderSeg(level, int(seg), vertices)...)
@@ -46,15 +55,97 @@ func renderSubsector(level *Level, idx int, vertices []Vertex) []Vertex {
 	return vertices
 }
 
-func renderSeg(level *Level, idx int, vertices []Vertex) []Vertex {
+func renderSeg(level *Level, idx int, vertices []Point3) []Point3 {
 	seg := level.Segs[idx]
-	return renderLinedef(level, int(seg.LineNum), vertices)
+	return renderLinedef(level, &seg, int(seg.LineNum), vertices)
 }
 
-func renderLinedef(level *Level, idx int, vertices []Vertex) []Vertex {
+func segSidedef(level *Level, seg *Seg, linedef *Linedef) *Sidedef {
+	if seg.Segside == 0 {
+		return &level.Sidedefs[linedef.SidedefRight]
+	} else {
+		if linedef.SidedefLeft == -1 {
+			return nil
+		}
+		return &level.Sidedefs[linedef.SidedefLeft]
+	}
+}
+
+func segOppositeSidedef(level *Level, seg *Seg, linedef *Linedef) *Sidedef {
+	if seg.Segside == 0 {
+		if linedef.SidedefLeft == -1 {
+			return nil
+		}
+		return &level.Sidedefs[linedef.SidedefLeft]
+	} else {
+		return &level.Sidedefs[linedef.SidedefRight]
+	}
+}
+
+func renderLinedef(level *Level, seg *Seg, idx int, vertices []Point3) []Point3 {
 	linedef := level.Linedefs[idx]
-	vertices = append(vertices, level.Vertexes[linedef.VertexStart])
-	vertices = append(vertices, level.Vertexes[linedef.VertexEnd])
+
+	sidedef := segSidedef(level, seg, &linedef)
+	if sidedef == nil {
+		return vertices
+	}
+	sector := level.Sectors[sidedef.SectorRef]
+
+	oppositeSidedef := segOppositeSidedef(level, seg, &linedef)
+
+	start := level.Vertexes[linedef.VertexStart]
+	end := level.Vertexes[linedef.VertexEnd]
+
+	upperTexture := ToString(sidedef.UpperTexture)
+	middleTexture := ToString(sidedef.MiddleTexture)
+	lowerTexture := ToString(sidedef.LowerTexture)
+
+	if upperTexture != "-" && oppositeSidedef != nil {
+		oppositeSector := level.Sectors[oppositeSidedef.SectorRef]
+
+		vertices = append(vertices, Point3{X: -start.XCoord, Y: sector.CeilingHeight, Z: start.YCoord})
+		vertices = append(vertices, Point3{X: -start.XCoord, Y: oppositeSector.CeilingHeight, Z: start.YCoord})
+
+		vertices = append(vertices, Point3{X: -start.XCoord, Y: oppositeSector.CeilingHeight, Z: start.YCoord})
+		vertices = append(vertices, Point3{X: -end.XCoord, Y: oppositeSector.CeilingHeight, Z: end.YCoord})
+
+		vertices = append(vertices, Point3{X: -end.XCoord, Y: oppositeSector.CeilingHeight, Z: end.YCoord})
+		vertices = append(vertices, Point3{X: -end.XCoord, Y: sector.CeilingHeight, Z: end.YCoord})
+
+		vertices = append(vertices, Point3{X: -end.XCoord, Y: sector.CeilingHeight, Z: end.YCoord})
+		vertices = append(vertices, Point3{X: -start.XCoord, Y: sector.CeilingHeight, Z: start.YCoord})
+	}
+
+	if middleTexture != "-" {
+		vertices = append(vertices, Point3{X: -start.XCoord, Y: sector.FloorHeight, Z: start.YCoord})
+		vertices = append(vertices, Point3{X: -start.XCoord, Y: sector.CeilingHeight, Z: start.YCoord})
+
+		vertices = append(vertices, Point3{X: -start.XCoord, Y: sector.CeilingHeight, Z: start.YCoord})
+		vertices = append(vertices, Point3{X: -end.XCoord, Y: sector.CeilingHeight, Z: end.YCoord})
+
+		vertices = append(vertices, Point3{X: -end.XCoord, Y: sector.CeilingHeight, Z: end.YCoord})
+		vertices = append(vertices, Point3{X: -end.XCoord, Y: sector.FloorHeight, Z: end.YCoord})
+
+		vertices = append(vertices, Point3{X: -end.XCoord, Y: sector.FloorHeight, Z: end.YCoord})
+		vertices = append(vertices, Point3{X: -start.XCoord, Y: sector.FloorHeight, Z: start.YCoord})
+	}
+
+	if lowerTexture != "-" && oppositeSidedef != nil {
+		oppositeSector := level.Sectors[oppositeSidedef.SectorRef]
+
+		vertices = append(vertices, Point3{X: -start.XCoord, Y: sector.FloorHeight, Z: start.YCoord})
+		vertices = append(vertices, Point3{X: -start.XCoord, Y: oppositeSector.FloorHeight, Z: start.YCoord})
+
+		vertices = append(vertices, Point3{X: -start.XCoord, Y: oppositeSector.FloorHeight, Z: start.YCoord})
+		vertices = append(vertices, Point3{X: -end.XCoord, Y: oppositeSector.FloorHeight, Z: end.YCoord})
+
+		vertices = append(vertices, Point3{X: -end.XCoord, Y: oppositeSector.FloorHeight, Z: end.YCoord})
+		vertices = append(vertices, Point3{X: -end.XCoord, Y: sector.FloorHeight, Z: end.YCoord})
+
+		vertices = append(vertices, Point3{X: -end.XCoord, Y: sector.FloorHeight, Z: end.YCoord})
+		vertices = append(vertices, Point3{X: -start.XCoord, Y: sector.FloorHeight, Z: start.YCoord})
+	}
+
 	return vertices
 }
 
@@ -70,7 +161,7 @@ func pointOnSide(point *Point, node *Node) int {
 	return 1
 }
 
-func traverseBsp(level *Level, point *Point, idx int, visibility bool, vertices []Vertex) []Vertex {
+func traverseBsp(level *Level, point *Point, idx int, visibility bool, vertices []Point3) []Point3 {
 	if idx&subsectorBit == subsectorBit {
 		if idx == -1 {
 			return renderSubsector(level, 0, vertices)
@@ -85,8 +176,8 @@ func traverseBsp(level *Level, point *Point, idx int, visibility bool, vertices 
 		side := pointOnSide(point, &node)
 		return traverseBsp(level, point, int(node.Child[side]), visibility, vertices)
 	} else {
-		left := traverseBsp(level, point, int(node.Child[0]), visibility, []Vertex{})
-		right := traverseBsp(level, point, int(node.Child[1]), visibility, []Vertex{})
+		left := traverseBsp(level, point, int(node.Child[0]), visibility, []Point3{})
+		right := traverseBsp(level, point, int(node.Child[1]), visibility, []Point3{})
 		vertices = append(vertices, left...)
 		vertices = append(vertices, right...)
 		return vertices
@@ -179,6 +270,8 @@ func game(level *Level, position *Point) {
 
 	defer window.Destroy()
 
+	width, height := window.GetSize()
+
 	window.MakeContextCurrent()
 	glfw.SwapInterval(1)
 
@@ -190,13 +283,16 @@ func game(level *Level, position *Point) {
 	vbo := gl.GenBuffer()
 	vbo.Bind(gl.ARRAY_BUFFER)
 
-	vertices := traverseBsp(level, position, len(level.Nodes)-1, false, []Vertex{})
+	speed := float32(5.0)
 
+	eye := mgl32.Vec3{float32(-position.X), 0.0, float32(position.Y)}
+	direction := mgl32.Vec3{0.0, 0.0, 1.0}
+
+	vertices := traverseBsp(level, position, len(level.Nodes)-1, false, []Point3{})
 	vbo_data := []float32{}
 	for _, vertex := range vertices {
-		vbo_data = append(vbo_data, float32(vertex.XCoord)/32767.0*10.0, float32(vertex.YCoord)/32767.0*10.0, 0.0)
+		vbo_data = append(vbo_data, float32(vertex.X), float32(vertex.Y), float32(vertex.Z))
 	}
-
 	gl.BufferData(gl.ARRAY_BUFFER, len(vbo_data)*4, vbo_data, gl.STATIC_DRAW)
 
 	vertex_shader := gl.CreateShader(gl.VERTEX_SHADER)
@@ -217,18 +313,30 @@ func game(level *Level, position *Point) {
 
 	program.BindFragDataLocation(0, "outColor")
 	program.Link()
-	program.Use()
 	defer program.Delete()
 
-	positionAttrib := program.GetAttribLocation("position")
-	positionAttrib.AttribPointer(3, gl.FLOAT, false, 0, nil)
-	positionAttrib.EnableArray()
-	defer positionAttrib.DisableArray()
+	vertexAttrib := program.GetAttribLocation("vertex")
+	defer vertexAttrib.DisableArray()
+
+	matrixID := program.GetUniformLocation("MVP")
 
 	gl.ClearColor(0.3, 0.3, 0.3, 1.0)
 
 	for !window.ShouldClose() {
 		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+
+		program.Use()
+
+		vertexAttrib.AttribPointer(3, gl.FLOAT, false, 0, nil)
+		vertexAttrib.EnableArray()
+
+		projection := mgl32.Perspective(64.0, float32(width)/float32(height), 1.0, 10000.0)
+		view := mgl32.LookAt(eye.X(), eye.Y(), eye.Z(), eye.X()+direction.X(), eye.Y()+direction.Y(), eye.Z()+direction.Z(), 0.0, 1.0, 0.0)
+		model := mgl32.Ident4()
+		mvp := projection.Mul4(view).Mul4(model)
+
+		matrixID.UniformMatrix4fv(false, mvp)
+
 		gl.DrawArrays(gl.LINES, 0, len(vbo_data))
 
 		window.SwapBuffers()
@@ -236,6 +344,18 @@ func game(level *Level, position *Point) {
 
 		if window.GetKey(glfw.KeyEscape) == glfw.Press {
 			window.SetShouldClose(true)
+		}
+		if window.GetKey(glfw.KeyUp) == glfw.Press {
+			eye = eye.Add(direction.Mul(speed))
+		}
+		if window.GetKey(glfw.KeyDown) == glfw.Press {
+			eye = eye.Sub(direction.Mul(speed))
+		}
+		if window.GetKey(glfw.KeyLeft) == glfw.Press {
+			direction = mgl32.QuatRotate(0.1, mgl32.Vec3{0.0, 1.0, 0.0}).Rotate(direction)
+		}
+		if window.GetKey(glfw.KeyRight) == glfw.Press {
+			direction = mgl32.QuatRotate(-0.1, mgl32.Vec3{0.0, 1.0, 0.0}).Rotate(direction)
 		}
 	}
 }
